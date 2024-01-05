b00a0075290211a717e5233d04a3eaa36ca7767b
Ulvi <unovruzov@yahoo.com>
2024-01-05 23:58:56 +0400

    Jan 4 class is over

---
+++ src/test/resources/querries/day04_docuport/task01_docuport.sql
@@ -0,0 +1,13 @@
+SELECT * FROM DOCUMENT.users
+WHERE first_name = 'Ramiz';
+
+
+SELECT * FROM DOCUMENT.users
+WHERE email_address = 'Aksdjkdj@gmail.com';
+
+
+SELECT * FROM DOCUMENT.users
+WHERE supervisor_user_id IS NOT NULL;
+
+SELECT * FROM DOCUMENT.users
+WHERE users.department_id IS NOT NULL;
